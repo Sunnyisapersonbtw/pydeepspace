af705fd74f13b4353bbec89e2fc4529fbea9283b
Matthew13479 <matthew.egan@thedropbears.org.au>
2019-01-14 19:47:29 +1100

    Added basics for recruits to start

--- robot.py
+++ robot.py
@@ -3,11 +3,23 @@
 import magicbot
 import wpilib
 
+from automations.hatch import HatchController
+from components.hatch import Hatch
+
 
 class Robot(magicbot.MagicRobot):
+
+    hatch: Hatch
+
     def createObjects(self):
         """Create motors and stuff here."""
-        pass
+        self.top_puncher = wpilib.Solenoid(0)
+        self.left_puncher = wpilib.Solenoid(1)
+        self.right_puncher = wpilib.Solenoid(2)
+        self.actuator_arm = wpilib.Solenoid(3)
+
+        self.joystick = wpilib.Joystick(0)
+        self.gamepad = wpilib.XboxController(1)
 
     def teleopInit(self):
         """Initialise driver control."""
@@ -15,7 +27,11 @@ class Robot(magicbot.MagicRobot):
 
     def teleopPeriodic(self):
         """Allow the drivers to control the robot."""
-        pass
+        if self.gamepad.getBButtonPressed():
+            self.hatch.punch()
+
+        if self.gamepad.getAButtonPressed():
+            self.hatch.retract()
 
 
 if __name__ == "__main__":
