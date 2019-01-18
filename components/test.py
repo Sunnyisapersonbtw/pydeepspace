96810fed6c0508a04d9a50e82175e714ae52a5f5
Sunny Yang <sunny.yang4@education.nsw.gov.au>
2019-01-18 16:12:32 +1100

    Added testing features

---
+++ components/test.py
@@ -0,0 +1,5 @@
+import ctre
+import wpilib
+
+class Test:
+    self.test = ctre.TalonSRX(3)
--- robot.py
+++ robot.py
@@ -2,20 +2,31 @@
 
 import magicbot
 import wpilib
+import ctre
+
+from components.test import Test
 
 
 class Robot(magicbot.MagicRobot):
+
     def createObjects(self):
         """Create motors and stuff here."""
-        pass
+        self.Controller = wpilib.Xboxcontroller(0)
+        self.Motor = ctre.TalonSRX(1)
+        self.Button = wpilib.kA(2)
 
     def teleopInit(self):
         """Initialise driver control."""
-        pass
 
     def teleopPeriodic(self):
         """Allow the drivers to control the robot."""
-        pass
+    def getAButtonPressed(self) -> bool:
+        if self.Button == self.getRawButtonPressed(self.Button.kA):
+            set self.Motor(10)
+        else self.Motor_Output = 0
+        
+
+        
 
 
 if __name__ == "__main__":
